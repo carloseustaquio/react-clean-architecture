e14d5ca73d4bfd43e3c0689dabe90c8b24d947cb
Carlos Eustáquio <caeu227@gmail.com>
2020-12-25 16:46:02 -0300

    feat: ensure login show error if email is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -19,14 +19,20 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     email: "",
     password: "",
     errors: {
-      email: "Campo obrigatório",
+      email: "",
       password: "Campo obrigatório",
       form: "",
     },
   });
 
   useEffect(() => {
-    validation.validate("email", state.email);
+    setState({
+      ...state,
+      errors: {
+        ...state.errors,
+        email: validation.validate("email", state.email),
+      },
+    });
   }, [state.email]);
 
   useEffect(() => {
