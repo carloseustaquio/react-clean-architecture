6477794ffea7dff48ad56662d520533eadbfdb62
Carlos Eustáquio <caeu227@gmail.com>
2020-12-26 21:47:30 -0300

    fix: ensure email validation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -4,7 +4,7 @@ import faker from "faker";
 
 const makeSut = () => new EmailValidation(faker.database.column());
 
-describe("EmailValidstion", () => {
+describe("EmailValidation", () => {
   test("Should return error if email is invalid", () => {
     const sut = makeSut();
     const error = sut.validate(faker.random.word());
@@ -14,6 +14,12 @@ describe("EmailValidstion", () => {
   test("Should return falsy if email is valid", () => {
     const sut = makeSut();
     const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
+  });
+
+  test("Should return falsy if email is empty", () => {
+    const sut = makeSut();
+    const error = sut.validate("");
     expect(error).toBeFalsy();
   });
 });
