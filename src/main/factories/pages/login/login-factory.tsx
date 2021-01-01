27ad5f1241d7c2b701585769eb66f390a49b2026
Carlos Eustáquio <caeu227@gmail.com>
2021-01-01 00:25:02 -0300

    feat: update Login composition and add LocalSaveAccessToken

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React, { ReactElement } from "react";
 import { makeLoginValidation } from "./login-validation-factory";
 import { makeRemoteAuthentication } from "@/main/factories/usecases/authentication/remote-authentication-factory";
 import { Login } from "@/presentation/pages";
+import { makeLocalSaveAccessToken } from "@/main/factories/usecases/save-access-token/local-save-access-token-factory";
 
 export const makeLogin = (): ReactElement => {
   return (
     <Login
       validation={makeLoginValidation()}
       authentication={makeRemoteAuthentication()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   );
 };
