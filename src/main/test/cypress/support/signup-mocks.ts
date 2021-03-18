33394f1d14b7076f89764dccb448561e21fb02c7
Carlos Eustáquio <caeu227@gmail.com>
2021-03-17 22:45:26 -0300

    fix: mockOk url

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -8,7 +8,7 @@ export const mockUnexpectedError = (): void =>
   Helper.mockUnexpectedError(/signup/, "POST");
 
 export const mockOk = (): void =>
-  Helper.mockOk(/accessToken/, "POST", { accessToken: faker.random.uuid() });
+  Helper.mockOk(/signup/, "POST", { accessToken: faker.random.uuid() });
 
 export const mockInvalidData = (): void =>
   Helper.mockOk(/signup/, "POST", { invalid: faker.random.uuid() });
