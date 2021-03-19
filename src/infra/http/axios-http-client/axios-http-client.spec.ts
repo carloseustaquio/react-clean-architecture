785f15f8cfe42796b4d856b1ebe994cee1bbbcc9
Carlos Eustáquio <caeu227@gmail.com>
2021-03-19 20:50:56 -0300

    refactor: improve test names

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -17,23 +17,25 @@ const makeSut = (): SutTypes => {
 };
 
 describe("AxiosHttpClient", () => {
-  test("should call Axios with correct values", async () => {
-    const { sut, mockedAxios } = makeSut();
-    const request = mockPostRequest();
-    sut.post(request);
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
-  });
-  test("should return the correct statusCode and body", async () => {
-    const { sut, mockedAxios } = makeSut();
-    const promise = sut.post(mockPostRequest());
-    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
-  });
-  test("should return the correct statusCode and body on failure", async () => {
-    const { sut, mockedAxios } = makeSut();
-    mockedAxios.post.mockRejectedValueOnce({
-      response: mockHttpResponse(),
+  describe("post", () => {
+    test("should call axios.post with correct values", async () => {
+      const { sut, mockedAxios } = makeSut();
+      const request = mockPostRequest();
+      sut.post(request);
+      expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
     });
-    const promise = sut.post(mockPostRequest());
-    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+    test("should return correct response on axios.post", async () => {
+      const { sut, mockedAxios } = makeSut();
+      const promise = sut.post(mockPostRequest());
+      expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+    });
+    test("should return correct error on axios.post", async () => {
+      const { sut, mockedAxios } = makeSut();
+      mockedAxios.post.mockRejectedValueOnce({
+        response: mockHttpResponse(),
+      });
+      const promise = sut.post(mockPostRequest());
+      expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+    });
   });
 });
