c42413e7125f3d9cfaae0800d678f30cc99765e1
Carlos Eustáquio <caeu227@gmail.com>
2021-04-03 17:46:47 -0300

    feat: ensure PrivateRoute render component if token is provided

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -4,13 +4,17 @@ import { makeLogin } from "@/main/factories/pages/login/login-factory";
 import { makeSignUp } from "@/main/factories/pages/signup/signup-factory";
 import { SurveyList } from "@/presentation/pages";
 import { ApiContext } from "@/presentation/contexts";
-import { setCurrentAccountAdapter } from "@/main/adapters/current-account-adapter";
+import {
+  getCurrentAccountAdapter,
+  setCurrentAccountAdapter,
+} from "@/main/adapters/current-account-adapter";
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider
       value={{
         setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter,
       }}
     >
       <BrowserRouter>
