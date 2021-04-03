af93278b2620eef70caee0fe5b4b644f243164a5
Carlos Eustáquio <caeu227@gmail.com>
2021-04-03 15:42:14 -0300

    refactor: move router to main layer

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Router } from "./router/router";
 export { default as Footer } from "./footer/footer";
 export { default as FormStatus } from "./form-status/form-status";
 export { default as Input } from "./input/input";
