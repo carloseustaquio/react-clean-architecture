ca0e2647624b7fc7db875cdfd3cc139168b66b54
Carlos Eustáquio <caeu227@gmail.com>
2021-04-07 21:51:33 -0300

    refactor: export files from index

--- src/main/factories/usecases/add-account/remote-add-account-factory.ts
+++ src/main/factories/usecases/add-account/remote-add-account-factory.ts
@@ -1,6 +1,5 @@
-import { makeApiUrl } from "@/main/factories/http/api-url-factory";
+import { makeApiUrl, makeAxiosHttpClient } from "@/main/factories/http";
 import { AddAccount } from "@/domain/usecases";
-import { makeAxiosHttpClient } from "@/main/factories/http/axios-http-client-factory";
 import { RemoteAddAccount } from "@/data/usecases/add-account/remote-add-account";
 
 export const makeRemoteAddAccount = (): AddAccount => {
--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -1,7 +1,6 @@
 import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication";
-import { makeApiUrl } from "@/main/factories/http/api-url-factory";
+import { makeApiUrl, makeAxiosHttpClient } from "@/main/factories/http";
 import { Authentication } from "@/domain/usecases";
-import { makeAxiosHttpClient } from "@/main/factories/http/axios-http-client-factory";
 
 export const makeRemoteAuthentication = (): Authentication => {
   return new RemoteAuthentication(makeApiUrl("/login"), makeAxiosHttpClient());
