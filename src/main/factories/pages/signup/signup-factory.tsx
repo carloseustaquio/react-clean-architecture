160ec5cce7f12b32ab6001c46cb43c856b25c70f
Carlos Eustáquio <caeu227@gmail.com>
2021-04-07 21:53:40 -0300

    refactor: export files from index

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 import { makeSignupValidation } from "./signup-validation-factory";
 import { SignUp } from "@/presentation/pages";
-import { makeRemoteAddAccount } from "@/main/factories/usecases/add-account/remote-add-account-factory";
+import { makeRemoteAddAccount } from "@/main/factories/usecases";
 
 export const makeSignUp = (): ReactElement => {
   return (
