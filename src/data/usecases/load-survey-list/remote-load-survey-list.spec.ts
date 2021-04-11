97ab3b6de65900e36e3c9ac1f7bea3d03d638a0f
Carlos Eustáquio <caeu227@gmail.com>
2021-04-11 16:52:46 -0300

    test: ensure RemoteLoadSurveyList throws AccessDeniedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,7 +1,7 @@
 import faker from "faker";
 import { HttpGetClientSpy, mockRemoteSurveyList } from "@/data/test";
 import { RemoteLoadSurveyList } from "./remote-load-survey-list";
-import { UnexpectedError } from "@/domain/errors";
+import { AccessDeniedError, UnexpectedError } from "@/domain/errors";
 import { HttpStatusCode } from "@/data/protocols/http";
 
 type SutTypes = {
@@ -27,13 +27,13 @@ describe("RemoteLoadSurveyList", (): void => {
     expect(httpGetClientSpy.url).toBe(url);
   });
 
-  test("should throw UnexpectedError if HttpGetClient returns 400", async () => {
+  test("should throw AccessDeniedError if HttpGetClient returns 403", async () => {
     const { sut, httpGetClientSpy } = makeSut();
     httpGetClientSpy.response = {
-      statusCode: HttpStatusCode.badRequest,
+      statusCode: HttpStatusCode.forbidden,
     };
     const promise = sut.loadAll();
-    await expect(promise).rejects.toThrow(new UnexpectedError());
+    await expect(promise).rejects.toThrow(new AccessDeniedError());
   });
 
   test("should throw UnexpectedError if HttpGetClient returns 404", async () => {
