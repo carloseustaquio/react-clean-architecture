02fcd1b41f714ff3320310958c0d46366d7c5f4f
Carlos Eustáquio <caeu227@gmail.com>
2021-04-11 16:43:21 -0300

    feat: ensure render username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -5,7 +5,7 @@ import { ApiContext } from "@/presentation/contexts";
 import { useHistory } from "react-router";
 
 const Header: React.FC = () => {
-  const { setCurrentAccount } = useContext(ApiContext);
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext);
   const history = useHistory();
   const logout = (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
-          <span>Carlos Eustáquio</span>
+          <span data-testid="username">{getCurrentAccount().name}</span>
           <a data-testid="logout" href="#" onClick={logout}>
             Sair
           </a>
