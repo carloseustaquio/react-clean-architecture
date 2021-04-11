20f368022b0d8cce14bc7a8307c979b68f50b776
Carlos Eustáquio <caeu227@gmail.com>
2021-04-11 00:39:46 -0300

    test: ensure AuthorizeHttpGetClientDecorator not add headers if GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,25 +1,45 @@
-import { AuthorizeHttpGetClientDecorator } from "@/main/decorators"
-import { GetStorageSpy, mockGetRequest } from "@/data/test"
-
+import { AuthorizeHttpGetClientDecorator } from "@/main/decorators";
+import { GetStorageSpy, HttpGetClientSpy, mockGetRequest } from "@/data/test";
+import faker from "faker";
+import { HttpGetParams } from "@/data/protocols/http";
 
 type SutTypes = {
-  sut: AuthorizeHttpGetClientDecorator,
-  getStorageSpy: GetStorageSpy
-}
+  sut: AuthorizeHttpGetClientDecorator;
+  getStorageSpy: GetStorageSpy;
+  httpGetClientSpy: HttpGetClientSpy;
+};
 
 const makeSut = (): SutTypes => {
-  const getStorageSpy = new GetStorageSpy()
-  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+  const getStorageSpy = new GetStorageSpy();
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new AuthorizeHttpGetClientDecorator(
+    getStorageSpy,
+    httpGetClientSpy
+  );
   return {
     sut,
-    getStorageSpy
-  }
-}
+    getStorageSpy,
+    httpGetClientSpy,
+  };
+};
 
-describe('AuthorizeHttpGetClientDecorator', () => {
-  test('should call GetStorage with correct value', () => {
-    const {sut, getStorageSpy} = makeSut()
-    sut.get(mockGetRequest())
-    expect(getStorageSpy.key).toBe('account')
-  })
-})
+describe("AuthorizeHttpGetClientDecorator", () => {
+  test("should call GetStorage with correct value", async () => {
+    const { sut, getStorageSpy } = makeSut();
+    await sut.get(mockGetRequest());
+    expect(getStorageSpy.key).toBe("account");
+  });
+
+  test("should not add headers if getStorage is invalid", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    const request: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field: faker.random.words(),
+      },
+    };
+    await sut.get(request);
+    expect(httpGetClientSpy.url).toBe(request.url);
+    expect(httpGetClientSpy.headers).toBe(request.headers);
+  });
+});
