bbf3b0b7fffb3268483625267d2fba3b92a86ed1
Carlos Eustáquio <caeu227@gmail.com>
2021-04-11 15:36:38 -0300

    feat: ensure LocalStorageAdapter calls localStorage.deleteItem if value is not defined

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -4,11 +4,19 @@ export class LocalStorageAdapter implements SetStorage, GetStorage {
   private readonly localStoragePrefix = "4devs";
 
   set(key: string, value: Record<string, unknown>): void {
-    localStorage.setItem(`${this.localStoragePrefix}-${key}`, JSON.stringify(value));
-    return;
+    if (value) {
+      localStorage.setItem(
+        `${this.localStoragePrefix}-${key}`,
+        JSON.stringify(value)
+      );
+    } else {
+      localStorage.removeItem(`${this.localStoragePrefix}-${key}`);
+    }
   }
 
   get(key: string): any {
-    return JSON.parse(localStorage.getItem(`${this.localStoragePrefix}-${key}`))
+    return JSON.parse(
+      localStorage.getItem(`${this.localStoragePrefix}-${key}`)
+    );
   }
 }
